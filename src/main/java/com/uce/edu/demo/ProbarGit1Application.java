de2b7f249974643883e0a63cea52576d4fe56687
°°PRISCI NENITA°° <kevinjkevps4@gmail.com>
2022-09-08 23:31:52 -0500

    prueba2

---
+++ src/main/java/com/uce/edu/demo/ProbarGit1Application.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbarGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbarGit1Application.class, args);
+	}
+
+}
